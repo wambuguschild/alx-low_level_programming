fe4fda14b3b0f5accbcfee4e1555f2b52161f937
Jedidah <jedidahwambuguw@gmail.com>
2022-10-22 05:28:20 -0700

    task 5 more numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,7 +10,6 @@ void more_numbers(void)
 
 	for (a = 0; a <= 9; a++)
 	{
-
 		for (num = 0; num <= 14; num++)
 		{
 			if (num > 9)
@@ -19,6 +18,7 @@ void more_numbers(void)
 			}
 			_putchar((num % 10) + '0');
 		}
+
+		_putchar('\n');
 	}
-i	_putchar('\n');
 }
