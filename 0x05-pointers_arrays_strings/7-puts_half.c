67432bf5d7f84134b3d96e52cac38e598a1dc880
Jedidah <jedidahwambuguw@gmail.com>
2022-10-25 11:54:07 -0700

    task 7 half a string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string
+ * @str: input
+ */
+void puts_half(char *str)
+{
+	int l = 0;
+
+	while (*str != '\0')
+	{
+		l++;
+		str++;
+	}
+
+	str -= (l / 2);
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++
+	}
+	_putchar('\n');
+}
