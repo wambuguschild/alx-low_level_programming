0103e1eee7c2f2b7ea73271e50d40dfe0cfec4a3
Jedidah <jedidahwambuguw@gmail.com>
2022-10-28 12:41:36 -0700

    task 4 reverse array

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * reverse_array - functions that reverses the content of an array of intergers
+ * @a: input interger
+ * @n: input interger
+ * @n: no of elements in the array
+ */
+void reverse_array(int *a, int n)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < n; i++)
+	{
+		n--;
+		j = a[i];
+		a[i] = a[n];
+		a[n] = j;
+	}
+}
