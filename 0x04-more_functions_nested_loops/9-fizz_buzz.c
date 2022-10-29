7dbc7981b99a3e8ec37ef6d120aa818f86f284ec
Jedidah <jedidahwambuguw@gmail.com>
2022-10-29 01:55:03 -0700

    task 9 FizzBuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,15 +14,20 @@ int main(void)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
 			printf("FizzBuzz");
+
 		else if (i % 3 == 0 && i % 5 != 0)
 			printf("Fizz");
+
 		else if (i % 5 == 0 && i % 3 != 0)
 			printf("Buzz");
+
 		else if (i == 100)
 			printf("Buzz");
+
 		else
 			printf("%d", i);
 	}
 	printf("\n");
+
 	return (0);
 }
