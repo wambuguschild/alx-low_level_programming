33b8893fbefabf95c6e71070731bc776483986b5
Jedidah <jedidahwambuguw@gmail.com>
2022-11-02 23:19:26 +0300

    task 7

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -11,14 +11,14 @@ void print_diagsums(int *a, int size)
 {
 	int i;
 
-	unsigned int sum 1, sum2;
+	unsigned int sum1, sum2;
 
 	sum1 = 0;
 	sum2 = 0;
 
 	for (i = 0; i < size; i++)
 	{
-		sum1 += a[(size * 1) + i];
+		sum1 += a[(size * i) + i];
 		sum2 += a[(size * (i + 1)) - (i + 1)];
 	}
 
