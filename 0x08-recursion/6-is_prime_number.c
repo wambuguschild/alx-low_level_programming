6c7ab8a3115a1bdc70e5f37cde9cb90b4f1a7b12
Jedidah <jedidahwambuguw@gmail.com>
2022-11-04 00:46:23 +0300

    task 6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -9,7 +9,7 @@ int actual_prime(int n, int i);
  */
 int is_prime_number(int n)
 {
-	if (n <= 0)
+	if (n <= 1)
 		return (0);
 	return (actual_prime(n, n - 1));
 }
@@ -26,5 +26,5 @@ int actual_prime(int n, int i)
 		return (1);
 	if (n % i == 0 && i > 0)
 		return (0);
-	return (actual_prime(n, n - 1));
+	return (actual_prime(n, i - 1));
 }
