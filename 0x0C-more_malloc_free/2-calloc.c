84c912044cc7e4bac78c76d8d9b7938e60e0f822
Jedidah <jedidahwambuguw@gmail.com>
2022-11-13 16:21:26 +0300

    task 2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,5 +1,6 @@
 #include "holberton.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
  * _calloc - Allocates memory for an array of a certain number
@@ -7,27 +8,22 @@
  * @nmemb: The number of elements.
  * @size: The byte size of each array element.
  *
- * Return: If nmemb = 0, size = 0, or the function fails - NULL.
+ * Return: NULLif error,
  *         Otherwise - a pointer to the allocated memory.
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *mem;
-	char *filler;
-	unsigned int index;
+	int *p;
+	unsigned int i;
 
-	if (nmemb == 0 || size == 0)
+	if (nmemb <= 0; || size <= 0)
 		return (NULL);
 
-	mem = malloc(size * nmemb);
-
-	if (mem == NULL)
+	p = malloc(nmemb * size);
+	if (p == NULL)
 		return (NULL);
+	for (i = 0; i < nmemb; i++)
+		p[i] = 0;
 
-	filler = mem;
-
-	for (index = 0; index < (size * nmemb); index++)
-		filler[index] = '\0';
-
-	return (mem);
+	return (p);
 }
