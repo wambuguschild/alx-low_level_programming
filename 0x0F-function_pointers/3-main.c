c1da4126838607e22201a98fe95ae66066a1adfa
Jedidah <jedidahwambuguw@gmail.com>
2022-11-18 22:09:13 +0300

    task 3

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,41 +1,46 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "3-calc.h"
+#include <stdlib.h>
 
 /**
- * main - entry point
+ * main - program that performs simple operations
  * @argc: number of arguments
- * @argv: array of arguments
+ * @argv:array of arguments
+ *
  * Return: Always 0
  */
-int main(int argc, int argv)
+int main(int argc, char *argv[])
 {
-	int a, int b;
-	char *op;
+	int a, b, r;
+	char *o;
+	int (*func)(int, int);
 
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-
 	a = atoi(argv[1]);
-	op = argv[2];
 	b = atoi(argv[3]);
 
-	if (get_op_func(op) == NULL || op[1] != '\0')
+	func = get_op_func(argv[2]);
+
+	if (!func)
 	{
 		printf("Error\n");
 		exit(99);
 	}
 
-	if ((*op == '\' && b == 0) || (*op == ' % ' && b == 0))
+	o = *argv[2];
+
+	if ((o == '/' || o == '%') && b == 0)
 	{
 		printf("Error\n");
 		exit(100);
 	}
 
-	printf("%d\n", get_op_func(op)(a, b));
+	r = func(a, b);
+
+	printf("%d\n", r);
 
 	return (0);
 }
