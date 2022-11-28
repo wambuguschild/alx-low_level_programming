e309aa1d5f8aec814ddbbc0278b376e49265c7f0
Jedidah <jedidahwambuguw@gmail.com>
2022-11-29 00:05:35 +0300

    task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -12,10 +12,10 @@ size_t print_list(const list_t *h)
 
 	while (h != NULL) 
 	{
-		if (h->str = NULL)
+		if (h->str == NULL)
 			printf("[0] (nil)\n");
 		else
-			printf("[%u] %s\n", h->len, h->str);
+			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
 		s++;
 	}
